0d0c3ea09006a5261441a42f024e9fa1223638c7
lamvd <vuduclam2412@gmail.com>
2024-02-01 16:44:02 +0700

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,38 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :set_user, only: %i[show update]
+
     #GET /users/1
     def show
-        render json: User.find_by(id: params[:id])
+        render json: @user
     end
+    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: @user, status: :ok
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    #Only allow a trusted parameter "white list" through
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end
+    
 end
